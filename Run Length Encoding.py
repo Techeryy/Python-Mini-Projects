6ce5dd4d69913992d970c031e0e20236bb580619
Stephen Adams <stephenadams2006@icloud.com>
2024-06-13 19:47:10 +0000

    Added Run Length Encoding Algorithm

---
+++ Run Length Encoding.py
@@ -0,0 +1,17 @@
+# Simple run length encoding compression algorithm,
+# part of Techeryy/Python-Mini-Projects
+# Programmed By: Stephen Adams
+
+def rleCompression(string,count=1,encode=''):
+    value = string[0]
+    for i in range(len(string)-1):
+        if string[i+1] == value:
+            count += 1
+        else:
+            encode += (str(count) + string[i])
+            value = string[i+1]
+            count = 1
+    encode += (str(count) + string[-1:])
+    return encode
+
+print(rleCompression(input('Enter String: ')))
